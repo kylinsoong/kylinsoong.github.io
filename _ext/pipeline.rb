a9f9a787dd0783b195fc7e8f3f0d47d47bf23685
Kylin <kylinsoong.1214@gmail.com>
2014-07-08 16:05:18 +0800

    update init templete

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,6 +1,7 @@
 require 'zurb-foundation'
 
 Awestruct::Extensions::Pipeline.new do
+  helper Awestruct::Extensions::Partial
   # extension Awestruct::Extensions::Posts.new '/news'
   # extension Awestruct::Extensions::Indexifier.new
   # Indexifier *must* come before Atomizer
