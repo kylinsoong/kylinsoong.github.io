6b653e4795ed9bf868e05e2bb87d43ef8cfce567
Kylin <kylinsoong.1214@gmail.com>
2014-07-16 13:21:12 +0800

    add blog layout, tag, index

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,18 +1,14 @@
 require 'zurb-foundation'
 
 Awestruct::Extensions::Pipeline.new do
-  extension Awestruct::Extensions::Posts.new( '/blog' )
-  extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page=>10 )
-  extension Awestruct::Extensions::Tagger.new( :posts, '/index', '/blog/tags', :per_page=>10 )
-  extension Awestruct::Extensions::TagCloud.new( :posts, '/blog/tags/index.html', :layout=>:base, :title=>'Blog Tags' )
-  extension Awestruct::Extensions::Atomizer.new( :posts, '/blog.atom', :num_entries=>20 )
-  extension Awestruct::Extensions::IntenseDebate.new
+  extension Awestruct::Extensions::Posts.new('/blog', :posts)
+  extension Awestruct::Extensions::Paginator.new( :posts, '/blog/index', :per_page=>2 )
+  extension Awestruct::Extensions::Tagger.new( :posts, '/blog/index', '/blog/tags', :per_page=>2 )
+  extension Awestruct::Extensions::TagCloud.new( :posts, '/blog/tags/index.html', :per_page=>2 )
+  extension Awestruct::Extensions::Atomizer.new( :posts, '/blog/blog.atom', :num_entries=>5, :feed_title=>'Latest Posts')
   extension Awestruct::Extensions::Indexifier.new
-
-  helper Awestruct::Extensions::GoogleAnalytics
-  helper Awestruct::Extensions::Partial
-  # extension Awestruct::Extensions::Posts.new '/news'
-  # extension Awestruct::Extensions::Indexifier.new
   # Indexifier *must* come before Atomizer
   # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+
+  helper Awestruct::Extensions::GoogleAnalytics
 end
